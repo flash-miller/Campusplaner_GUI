140ba111f72acf869a87a93b42bd789fc1eb92ba
vanessath <50193896+vanessath@users.noreply.github.com>
2023-01-16 17:08:01 +0100

    Notification: Add component and data

---
+++ campusplaner/src/components/NotificationOverview.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-img class="tv-info" :src="require('@/assets/images/tv_info.jpg')" />
+    <div>
+        <h3>BENACHRICHTIGUNGEN</h3>
+        <div>
+            <p>HOCHSCHULE</p>
+            <div v-for="data in content.body">{{ data }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { json } from '@/assets/data/notificationData';
+export default {
+    data() {
+        return {
+            content: {
+                body: [
+                    [
+                        {
+                            "headline": "Klausuranmeldung",
+                            "text": "Ihr habt jetzt bis zum 08.01.2023 zeit euch für die Klausur anzumelden.",
+                            "date": "10.12.2022 13:45"
+                        },
+                        {
+                            "headline": "Verfügungstag",
+                            "text": "Morgen ist ein Verfügungstag und somit finden morgen keine Vorlesungen statt.",
+                            "date": "08.12.2022 10:00"
+                        }
+                    ]
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.tv-info {
+    width: 698px;
+}
+</style>
