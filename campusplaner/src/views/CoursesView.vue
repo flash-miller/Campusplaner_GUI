0f697df7698db9720f104feeb8d40f296692c7a6
Deupla <maartenp.jbs@gmx.de>
2023-01-17 22:16:56 +0100

    Courseview

--- campusplaner/src/views/CoursesView.vue
+++ campusplaner/src/views/CoursesView.vue
@@ -1,43 +1,87 @@
 <template>
-  <div class="about">
-    <h1>Deine Kursübersicht</h1>
-    <div class="listbox-area">
-  <div>
-	<input type="search" id="search" placeholder="Suche…"/>
-	
-    <ul id="ss_elem_list"
-        role="listbox"
-        aria-labelledby="ss_elem">
-        <v-card v-for ="course in getStudentCourses()">
-        <li id="ss_elem_Np" role="option">
-        {{course}}
-      </li>
-        </v-card>
-      
-    </ul>
-  </div>
-</div>
-  </div>
+  <v-row no-gutters>
+    <v-col cols="7" md="7" sm="12">
+      <v-card>
+        <div class="about">
+
+          <h1>Deine Kursübersicht</h1>
+          <div class="listbox-area">
+            <div>
+              <input type="search" id="search" placeholder="Suche…" />
+
+              <ul id="ss_elem_list" role="listbox" aria-labelledby="ss_elem">
+                <v-card v-for="course in getStudentCourses()">
+                  <li id="ss_elem_Np" role="option">
+                    {{ course }}
+                  </li>
+                </v-card>
+
+              </ul>
+            </div>
+          </div>
+        </div>
+      </v-card>
+    </v-col>
+    <v-col cols="5" md="5" sm="12">
+      <v-card outlined tile>
+        <h2>Systemnahe Programmierung</h2>
+        <p>Christian Uhlig</p>
+        <p>Mittwoch 8:00-9:15 Uhr</p>
+        <p>Klausur</p>
+        <br>
+        <p>zusätzliche Übung</p>
+        <p>ÜBUNG SYSTEMNAHE PROGRAMMIERUNG</p>
+        <p>Übung ist vorbedingung für weitere Module!</p>
+        <br>
+        <h2>BENACHRICHTIGUNGEN</h2>
+        <div class="termin">
+          <p>SYSTEMNAHE PROGRAMMIERUNG</p>
+          <p>HEUTE</p>
+        </div>
+        <div class="termin">
+          <p>Das Tutorium findet heute ab 15:30 Uhr in HS01 statt.</p>
+          <p>11:45</p>
+        </div>
+        <br>
+
+
+        <p>SYSTEMNAHE PROGRAMMIERUNG</p>
+        <p>Die Ergebnisse der Nachbereitung vom letzten Mittwoch stehen zur Verfügung. Es geht um dne Vorlesungsteil 4
+          “Listenstrukturen”.</p>
+          <br>
+        <h2>VORLESUNGSMATERIALEN</h2>
+      </v-card>
+    </v-col>
+  </v-row>
+
 </template>
 
 <script>
 import store from "../store";
 export default {
-    methods: {
-      getStudentCourses(){
-        return store.getters.getStudentCourses;
-      }
+  methods: {
+    getStudentCourses() {
+      return store.getters.getStudentCourses;
     }
   }
+}
 
 </script>
 
-<style>.listbox-area {
-    padding: 15px;
-    border: 1px solid #aaa;
-    border-radius: 19px;
-    background: #eee;
-    margin-right: 50%
+<style>
+
+.termin {
+  display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.listbox-area {
+  padding: 15px;
+  border: 1px solid #aaa;
+  border-radius: 19px;
+  background: #eee;
+  margin-right: 50%
 }
 
 [role="listbox"] {
@@ -52,8 +96,8 @@ export default {
   overflow-y: auto;
 }
 
-[role="listbox"] + *,
-.listbox-label + * {
+[role="listbox"]+*,
+.listbox-label+* {
   margin-top: 1em;
 }
 
@@ -62,7 +106,7 @@ export default {
   padding: 0;
 }
 
-[role="group"] > [role="presentation"] {
+[role="group"]>[role="presentation"] {
   display: block;
   margin: 0;
   padding: 0 0.5em;
@@ -120,7 +164,7 @@ button[aria-haspopup="listbox"][aria-expanded="true"]::after {
   content: "";
 }
 
-button[aria-haspopup="listbox"] + [role="listbox"] {
+button[aria-haspopup="listbox"]+[role="listbox"] {
   position: absolute;
   margin: 0;
   width: 9.5em;
@@ -133,12 +177,12 @@ button[aria-haspopup="listbox"] + [role="listbox"] {
   display: flex;
 }
 
-[role="toolbar"] > * {
+[role="toolbar"]>* {
   border: 1px solid #aaa;
   background: #ccc;
 }
 
-[role="toolbar"] > [aria-disabled="false"]:focus {
+[role="toolbar"]>[aria-disabled="false"]:focus {
   background-color: #eee;
 }
 
@@ -176,6 +220,7 @@ button[aria-disabled="true"] {
   clip: rect(1px, 1px, 1px, 1px);
   font-size: 14px;
   white-space: nowrap;
-}</style>
+}
+</style>
 
 
