db6a9afcd5dd8bfe6f2d0d9fed5319342b38fcc7
Nino-Julian Müller <51022442+flash-miller@users.noreply.github.com>
2023-01-18 13:36:35 +0100

    TODO's fast fertig

--- campusplaner/src/components/TodoComponent.vue
+++ campusplaner/src/components/TodoComponent.vue
@@ -1,11 +1,12 @@
 <template>
     <h1>TODO</h1>
-    <v-virtual-scroll>
         <v-row
             no-gutters
+            class="todoEntryRow newTodoRow"
         >
             <v-col
-                cols="2"
+                cols="1"
+                class="checkboxCol"
             >
                 <v-checkbox
                     class="todoCheckbox"
@@ -17,9 +18,10 @@
                 cols="6"
                 >
                 <v-text-field
-                    placeholder="neues todo"
+                    placeholder="Neues TODO"
                     v-model="newTodoText"
                     variant="underlined"
+                    class="textInput"
                     v-on:keyup.enter="addNewTodo()"
                 ></v-text-field>
             </v-col>
@@ -28,15 +30,33 @@
                 cols="3"
                 class="todoDateCol"
             >
-                <p class="todoDate">{{ newTodoDate.toLocaleString().split(',')[0].replaceAll('/','.') }}</p>
+                <!--<p class="todoDate">{{ newTodoDate.toLocaleString().split(',')[0].replaceAll('/','.') }}</p>-->
+                <Datepicker
+                    class="datePickerInput"
+                    v-model="newTodoDate"
+                />
+            </v-col>
+            <v-col
+                cols="1"
+                class="actionButton"
+            >
+                <v-btn
+                    block
+                    v-on:click="addNewTodo()"
+                >
+                    <v-icon>mdi-calendar-plus</v-icon>
+                </v-btn>
             </v-col>
         </v-row>
         <v-row
             no-gutters
-            v-for="todo in getTodos()"
+            class="todoEntryRow"
+            v-for="(todo, i) in getTodos()"
+            :key="refreshKey"
         >
             <v-col
-                cols="2"
+                cols="1"
+                class="checkboxCol"
             >
                 <v-checkbox
                     v-model="todo.checked"
@@ -57,42 +77,77 @@
                 cols="3"
                 class="todoDateCol"
             >
-                <p class="todoDate">{{ new Date(todo.time).toLocaleString().split(',')[0].replaceAll('/','.') }}</p>
+                <!--<p class="todoDate">{{ new Date(todo.time).toLocaleString().split(',')[0].replaceAll('/','.') }}</p>-->
+                <Datepicker
+                    class="datePickerInput"
+                    v-model="todo.time"
+                />
+            </v-col>
+            <v-col
+                cols="1"
+                class="actionButton"
+            >
+                <v-btn
+                    block
+                    v-on:click="removeTodo(i)"
+                >
+                    <v-icon>mdi-delete-outline</v-icon>
+                </v-btn>
             </v-col>
         </v-row>
-    </v-virtual-scroll>
 </template>
 
 <script>
 
 import store from "../store";
+import Datepicker from 'vue3-datepicker';
+import { ref, onMounted } from 'vue';
 
 export default {
     name: "TodoComponent",
+    components: { Datepicker },
     methods: {
+        forceRerender() {
+            this.refreshKey += 1;
+        },
         getTodos() {
             return store.getters.getStudentTodos;
         },
         addNewTodo() {
-
+            store.dispatch('addTodo', {text: this.newTodoText, checked: false, time: this.newTodoDate});
+            this.updateTodos();
+            this.newTodoText = "";
+            this.newTodoDate = new Date();
+            this.forceRerender();
         },
-        updateStudentData() {
-
+        removeTodo(index){
+            store.dispatch('removeTodo', {index: index});
+            this.updateTodos();
+            this.forceRerender();
+        },
+        updateTodos() {
+            store.dispatch('saveState');
         }
     },
     data: () => ({
         newTodoText: "",
+        refreshKey: 0,
         newTodoDate: new Date(),
+        picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
     })
 }
 
 </script>
 
-<style scoped>
+<style>
     h1 {
         font-family: "Barlow-SemiBold", Helvetica, Arial;
         font-weight: 600;
         color: #1C2764;
+    }
+
+    .textInput input::placeholder {
+        font-style: italic;
     }
 
     .todoDate {
@@ -102,5 +157,37 @@ export default {
     .todoDateCol {
         display:flex;
         justify-content: end;
+        padding-top: 15px !important;
+        margin-right: 5px;
+    }
+
+    .v3dp__datepicker {
+        width: 100px;
+    }
+
+    .datePickerInput {
+        width: 100px;
+        cursor: pointer;
+        padding: 5px;
+        border-style: solid;
+        border-color: lightgrey;
+        border-radius: 5px;
+    }
+
+    .checkboxCol {
+        margin-right: 15px;
+        padding-top: 7px !important;
+    }
+
+    .actionButton {
+        padding-top: 15px !important;
+    }
+
+    .todoEntryRow {
+        height: 60px;
+    }
+
+    input::-webkit-input-placeholder{
+        color: red;
     }
 </style>
--- campusplaner/src/store/index.js
+++ campusplaner/src/store/index.js
@@ -37,6 +37,12 @@ export default createStore({
     logout(state){
       state.studentData = {};
       state.isLoggedIn = false;
+    },
+    addTodo(state, payload) {
+      state.studentData.todos.unshift(payload);
+    },
+    removeTodo(state, payload) {
+      state.studentData.todos.splice(payload.index, 1);
     }
   },
   actions: {
@@ -66,6 +72,12 @@ export default createStore({
     },
     saveState({commit}){
       
+    },
+    addTodo({commit}, payload) {
+      commit('addTodo', payload);
+    },
+    removeTodo({commit}, payload) {
+      commit('removeTodo', payload);
     }
   },
   modules: {
