a27d7e24f82518ec17e711758ea020e76493d42b
vanessath <50193896+vanessath@users.noreply.github.com>
2023-01-19 06:05:34 +0100

    Kalendar

---
+++ campusplaner/src/components/calenderComponent.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-row>
+        <v-calendar ref="calendar" :now="today" :value="today" :events="events" color="primary"
+            type="week"></v-calendar>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        today: '2019-01-08',
+        events: [
+            {
+                name: 'Nachbesprechung - Mobile Commerce',
+                start: '2019-01-07 09:00',
+                end: '2019-01-07 10:00',
+            },
+            {
+                name: `Verfügungstag`,
+                start: '2019-01-10',
+            },
+            {
+                name: 'Abnahme - Systemnahe Programmierung',
+                start: '2019-01-09 12:30',
+                end: '2019-01-09 15:30',
+            },
+        ],
+    }),
+}
+</script>
+
+<style scoped>
+.my-event {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    border-radius: 2px;
+    background-color: #1867c0;
+    color: #ffffff;
+    border: 1px solid #1867c0;
+    font-size: 12px;
+    padding: 3px;
+    cursor: pointer;
+    margin-bottom: 1px;
+    left: 4px;
+    margin-right: 8px;
+    position: relative;
+}
+
+.my-event.with-time {
+    position: absolute;
+    right: 4px;
+    margin-right: 0px;
+}
+</style>
--- campusplaner/src/views/HomeView.vue
+++ campusplaner/src/views/HomeView.vue
@@ -1,51 +1,36 @@
 <template>
   <div class="desktopView">
-  <h1>Hallo {{ getFirstName() }}!</h1>
-  <v-row no-gutters>
-    <v-col
-      cols="7"
-      md="7"
-      sm="12"
-      >
-      <v-card>
-        <NotificationOverview />
-      </v-card>
-    </v-col>
-    <v-col
-      cols="5"
-      md="5"
-      sm="12"
-    >
-      <v-card outlined tile class="todoWrapper">
-        <TodoComponent/>
-      </v-card>
-    </v-col>
-  </v-row>
+    <h1>Hallo {{ getFirstName() }}!</h1>
+    <v-row no-gutters>
+      <v-col cols="7" md="7" sm="12">
+        <v-card>
+          <NotificationOverview />
+        </v-card>
+      </v-col>
+      <v-col cols="5" md="5" sm="12">
+        <calenderComponent />
+        <v-card outlined tile class="todoWrapper">
+          <TodoComponent />
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 
   <div class="mobileView">
     <h1>Hallo {{ getFirstName() }}!</h1>
-  <v-row no-gutters>
-    <v-col
-      cols="17"
-      md="10"
-      sm="10"
-      >
-      <v-card>
-        <NotificationOverview />
-      </v-card>
-      <v-card outlined tile class="todoWrapper">
-        <TodoComponent/>
-      </v-card>
-    </v-col>
-    <v-col
-      cols="5"
-      md="5"
-      sm="5"
-    >
-     
-    </v-col>
-  </v-row>
+    <v-row no-gutters>
+      <v-col cols="17" md="10" sm="10">
+        <v-card>
+          <NotificationOverview />
+        </v-card>
+        <v-card outlined tile class="todoWrapper">
+          <TodoComponent />
+        </v-card>
+      </v-col>
+      <v-col cols="5" md="5" sm="5">
+
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -56,12 +41,13 @@ import store from "../store";
 // Components
 import NotificationOverview from '@/components/NotificationOverview.vue';
 import TodoComponent from '@/components/TodoComponent.vue';
+import calenderComponent from '@/components/calenderComponent.vue';
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
-    NotificationOverview, TodoComponent
+    NotificationOverview, TodoComponent, calenderComponent
   },
 
   methods: {
@@ -73,9 +59,8 @@ export default defineComponent({
 </script>
 
 <style scoped>
-
 @media (max-width: 700px) {
-  .desktopView{
+  .desktopView {
     display: none;
   }
 }
@@ -83,11 +68,12 @@ export default defineComponent({
 
 
 @media (min-width: 701px) {
-  .mobileView{
+  .mobileView {
     display: none;
   }
-   .todoWrapper {
+
+  .todoWrapper {
     overflow: visible;
   }
-  }
+}
 </style>
